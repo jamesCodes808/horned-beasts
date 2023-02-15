ec1832432552bb49da73dc34ada40d3b35e2118f
jamesCodes808 <jamesian.r.solima@gmail.com>
2023-02-14 16:02:31 -0800

    adds bootstrap responsive layout, interactivity, favorites tracker

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Header';
 import Footer from './Footer';
 import Main from './Main';
-import HornedBeast from './HornedBeast';
-
+import ThemeProvider from 'react-bootstrap/ThemeProvider'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+    <ThemeProvider
+      breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+      minBreakpoint="xxs"
+    >
+      <div className="App">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </ThemeProvider>
 
-    </div>
   );
 }
 
--- src/Main.js
+++ src/Main.js
@@ -1,46 +1,36 @@
 import React from 'react';
+import Data from './data.json'
 import HornedBeast from './HornedBeast';
-
-const hornedBeastsArray = [{
-    "_id": 1,
-    "image_url": "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
-    "title": "UniWhal",
-    "description": "A unicorn and a narwhal nuzzling their horns",
-    "keyword": "narwhal",
-    "horns": 1
-},
-
-{
-    "_id": 2,
-    "image_url": "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80",
-    "title": "Rhino Family",
-    "description": "Parent rhino with two babies",
-    "keyword": "rhino",
-    "horns": 2
-},
-
-{
-    "_id": 3,
-    "image_url": "https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg",
-    "title": "Unicorn Head",
-    "description": "Someone wearing a very silly unicorn head mask",
-    "keyword": "unicorn",
-    "horns": 1
-}]
+import ThemeProvider from 'react-bootstrap/ThemeProvider'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Main extends React.Component {
+
     render() {
         return (
-            <main>
-                {hornedBeastsArray.map(beast => (
-                    <HornedBeast
-                        key={beast._id}
-                        imageUrl={beast.image_url}
-                        description={beast.description}
-                        title={beast.title}
-                    />
-                ))}
-            </main>
+            <ThemeProvider
+                breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+                minBreakpoint="xxs"
+            >
+                <main>
+                    <Container fluid="lg">
+                        <Row className="justify-content-sm-center">
+                            {Data.map(beast => (
+                                <Col sm={12} md={6} lg={4} xl={4}>
+                                    <HornedBeast
+                                        key={beast._id}
+                                        imageUrl={beast.image_url}
+                                        description={beast.description}
+                                        title={beast.title}
+                                    />
+                                </Col>
+                            ))}
+                        </Row>
+                    </Container>
+                </main>
+            </ThemeProvider>
         )
     }
 }
